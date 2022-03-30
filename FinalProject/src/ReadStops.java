623bace6c1958e3ed11120d8050d008771a23e96
aislingmurray <aislingmurray@localhost>
2022-03-30 11:02:05 +0100

    Reading input file in

---
+++ FinalProject/src/ReadStops.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ReadStops {
+	int stopId;
+	int stopCode;
+	String stopName;
+	int locationType;
+	int stopLatitude;
+	int stopLongitude;
+	int parentStation;
+	
+	ReadStops(String filename){
+		readStops(filename);
+	}
+	
+	public void readStops(String filename) {
+		try {
+			Scanner scanner = new Scanner(filename);
+			
+		}catch(Exception e){
+			
+		}
+	}
+	
+	
+
+}
