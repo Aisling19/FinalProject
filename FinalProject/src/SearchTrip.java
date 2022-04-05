480fc582a21ff06849bd54f7078950b5e1eaf51e
aislingmurray <aislingmurray@aislings-mbp>
2022-04-05 14:46:56 +0100

    My search trip class finished but may not work yet

--- FinalProject/src/SearchTrip.java
+++ FinalProject/src/SearchTrip.java
@@ -3,7 +3,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-
+import java.util.Arrays;
 import java.util.List;
 
 /* 3. Searching for all trips with a given arrival time, returning full details of all trips matching the
@@ -13,16 +13,72 @@ will need to remove all invalid times, e.g., there are times in the file that st
 clearly invalid. Maximum time allowed is 23:59:59. 
 */
 public class SearchTrip {
+	List<StopTime> listOfStopTimes = new ArrayList<StopTime>();
 	
-	int numberOfValidEntries;
 	
-	public List<StopTime> matchedTrips(String filename) throws IOException {
+	
+	public void searchForTrips(String arrivalTime) throws IOException {
 		
-		numberOfValidEntries = 0;
-		List<StopTime> listOfStopTimes = new ArrayList<StopTime>();
+		List<StopTime> matchedStopTimes = new ArrayList<StopTime>();
+		
+		listOfStopTimes = getListOfValidTrips();
+		matchedStopTimes = matchArrivalTime(arrivalTime);
+		
+		
+		//If there are no matching trips
+        if (matchedStopTimes == null)
+        {
+            System.out.println("Unfortunately there are no trips with the given arrival time of " + arrivalTime);
+            return;
+        }
+        
+        StopTime[]  arrayStopTime = new StopTime[matchedStopTimes.size()];
+        
+        matchedStopTimes.toArray(arrayStopTime);
+        
+        // now sort the array according to their trip id's, using the merge sort from assignment 1
+        StopTime [] sortedStopTimes = new StopTime[arrayStopTime.length];
+        sortedStopTimes = mergeSortRecursive(arrayStopTime);
+        
+        // convert the sorted array back to an array list of objects
+        matchedStopTimes = Arrays.asList(sortedStopTimes);
+        
+        System.out.println("The trips that match with the inputed arrival time of " + arrivalTime + "are as follows:");
+        for(int i =0; i < matchedStopTimes.size(); i++) {
+        	System.out.println("   Trip id: " + matchedStopTimes.get(i).trip_id
+        			+ "\n   Arrival Time: " + matchedStopTimes.get(i).arrival_time
+        			+ "\n   Departure Time: " + matchedStopTimes.get(i).departure_time
+        			+ "\n   Stop id: " + matchedStopTimes.get(i).stop_id
+        			+ "\n   Stop Sequence: " + matchedStopTimes.get(i).stop_sequence
+        			+ "\n   Stop Headsign: " + matchedStopTimes.get(i).stop_headsign
+        			+ "\n   Pickup Type: " + matchedStopTimes.get(i).pickup_type
+        			+ "\n   Dropoff Type: " + matchedStopTimes.get(i).drop_off_type
+        			+ "\n   Shape of Distance Travelled: " + matchedStopTimes.get(i).shape_dist_travelled);
+        	System.out.println("-----------------");
+        }
+        
+		
+	}
+	
+	public List<StopTime> matchArrivalTime(String arrivalTime) {
+		List<StopTime> matchedStopTimes = new ArrayList<StopTime>();
+		for(int i =0; i< listOfStopTimes.size(); i++) {
+			if(arrivalTime.equals(listOfStopTimes.get(i).arrival_time) ) {
+				matchedStopTimes.add(listOfStopTimes.get(i));
+			}
+		}
+		return matchedStopTimes;
+	}
+	
+	
+	// this function creates an array list of all the valid entries
+	public List<StopTime> getListOfValidTrips() throws IOException {
+		
+		
+		
 		
 		//Buffered reader reads each line from file and stop_times
-		BufferedReader bufReader = new BufferedReader(new FileReader(filename));
+		BufferedReader bufReader = new BufferedReader(new FileReader("stop_times.txt"));
         //ignore first line
         bufReader.readLine();
      
@@ -45,12 +101,10 @@ public class SearchTrip {
             
             String departure_time = variables[2].replaceAll("\\s+", "0");
 
-            if(validTimeFormat(arrival_time) == true && validTimeFormat(departure_time) == true) {
-            	numberOfValidEntries ++;
+            if(validTimeFormat(arrival_time) != true && validTimeFormat(departure_time) != true) {
+            	continue; // skip this iteration of the loop if this is not a valid time as then it wont be added to the list
             }
-            else {
-            	continue; // skip this iteration of loop as not a valid time
-            }
+            
 
             
             
@@ -85,7 +139,7 @@ public class SearchTrip {
 
             
             
-            numberOfValidEntries++; // increment the number of valid entries
+             
 
             
         }
