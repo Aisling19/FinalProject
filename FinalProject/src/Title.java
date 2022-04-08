97cf59e6ca808d53953aca529ca950d615fd2102
aislingmurray <aislingmurray@aislings-mbp>
2022-04-08 20:44:15 +0100

    Graphics for interface

---
+++ FinalProject/src/Title.java
@@ -0,0 +1,27 @@
+
+
+public class Title {
+	public static void printHeader() {
+		System.out.println("--------------------------------------------");
+		System.out.println("|     Welcome to the Bus Management System  |");
+		System.out.println("|     For the Vancouver Bus Routes          | ");
+		System.out.println("--------------------------------------------");
+		
+	}
+	
+	public static void printSelection() {
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("||| The following function has four options |||");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("\n 1. To find the shortest path between two bus stops");
+		System.out.println("\n 2. To search for a bus stop by full name or by the first few characters in the name");
+		System.out.println("\n 3. To search for all trips with a given arrival time");
+		System.out.println("\n 4. An option to exit the programme");
+		System.out.println("");
+	}
+	
+
+}
